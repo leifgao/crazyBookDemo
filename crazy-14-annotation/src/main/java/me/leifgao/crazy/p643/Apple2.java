0d4350d8921156c56910c853d684830ed1bdf756
leifgao <leifgao@outlook.com>
2017-08-27 23:26:28 +0800

    注解章节

--- crazy-14-annotation/src/main/java/me/leifgao/crazy/p643/Apple2.java
+++ crazy-14-annotation/src/main/java/me/leifgao/crazy/p643/Apple2.java
@@ -1,10 +1,22 @@
 package me.leifgao.crazy.p643;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leif on 2017/8/27
  */
 public class Apple2 extends Fruit{
+
+    @Override
+    @Deprecated
     public void info() {
         System.out.println();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        new Apple2().info();
+        List<String> myList = new ArrayList();
     }
 }
